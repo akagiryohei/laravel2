1ba35fb71db97c2db2d442f4e9ce26b587aee09b
akagiryohei <abbbbbbbb35@gmail.com>
2023-05-27 15:16:24 +0900

    20230527-テーブル機能-完了

---
+++ app/database/migrations/2023_05_22_114807_create__products_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('Products', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('product_name','255');
+            $table->string('money','100');
+            $table->tinyInteger('del_flg')->default(0);
+            $table->string('img','255');
+
+
+
+
+
+
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('Products');
+    }
+}
